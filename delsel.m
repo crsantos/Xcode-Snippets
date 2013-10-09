e2fe9b3a6995d3828885081a6f18deb2d19d7244
Carlos Ricardo Santos <carlosricardosantos@gmail.com>
2013-10-09 08:58:53 +0100

    Delegate presence check and selector

---
+++ delsel.m
@@ -0,0 +1,10 @@
+// Delegate presence check and its selector
+// Checks if a delegate exists and responds to a particular selector
+//
+// Platform: iOS
+// Language: Objective-C
+// Completion Scope: Class Implementation
+
+if (_delegate && [_delegate respondsToSelector:@selector(<#selector#>)]) {
+    <#statements#>
+}
