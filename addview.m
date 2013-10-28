0befe3a1c5b7ea5e530ed7471fa1d701b5bd6a08
Carlos Ricardo Santos <carlosricardosantos@gmail.com>
2013-10-28 12:37:29 +0000

    Add view as subview

---
+++ addview.m
@@ -0,0 +1,9 @@
+// Documents UIView add
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Code Expression
+
+UIView * view = [[UIView alloc] initWithFrame:<#(CGRect)#>];
+[self.view addSubview:view];
+[view release]
