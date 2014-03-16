e9485c4f7b2c2ea05c59b978e090c328e17a0820
Carlos Ricardo Santos <carlosricardosantos@gmail.com>
2014-03-16 17:52:05 +0000

    Added nsoptions and nsenum snippets

---
+++ nsenum.m
@@ -0,0 +1,9 @@
+// Documents Enum creation
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Top Level
+
+typedef NS_ENUM(NSInteger, <#NameOfEnumeration#>) {
+        <#EnumOption1#>,
+};
---
+++ nsoptions.m
@@ -0,0 +1,11 @@
+// Documents NS_OPTIONS bitwise mask creation
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Top Level
+
+typedef NS_OPTIONS(NSUInteger, <#NameOfEnumeration#>) {
+        <#Option1#>   = 0,
+        <#Option2#>   = 1 << 0,
+        <#Option3#>   = 1 << 1,
+};
